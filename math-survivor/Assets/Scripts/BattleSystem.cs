10f463e858419e391c69b606534a5227938044de
mad-syir <musyir21@gmail.com>
2023-06-06 12:51:10 +0800

    File fixed

---
+++ math-survivor/Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+    public BattleState state;
+    private Unit playerUnit;
+    private Unit enemyUnit;
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;//finding the object
+
+    public Transform playerBattleStation;//location of the object
+    public Transform enemyBattleStation; //for good practice, make another gameobject that is a parent to the character to setup battlestation
+
+    public Text dialogueText;
+    void Start()
+    {
+        state = BattleState.START;
+        SetupBattle();
+    }
+
+    public void SetupBattle()
+    {
+        GameObject playerObj = Instantiate(playerPrefab, playerBattleStation); //getting reference of this instance
+        playerUnit = playerObj.GetComponent<Unit>();
+
+        GameObject enemyObj = Instantiate(enemyPrefab, enemyBattleStation);
+        enemyUnit = enemyObj.GetComponent<Unit>();
+
+        dialogueText.text = "An "+enemyUnit.name + " approaches..";
+    }
+}
---
+++ math-survivor/Assets/Scripts/Unit.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string name;
+    public int damage;
+    public int maxHP;
+    public int currentHP;
+
+}
